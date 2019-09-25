b10042c96cab9689928d3823b4fc9c37b1f0b3f8
Stas <workunow_stas@mail.ru>
2019-09-25 09:51:56 +0400

    Готовая

---
+++ src/main/java/Burger.java
@@ -0,0 +1,10 @@
+public class Burger {
+    String name;//название
+    String picture;// картинка, как выглядит бургер
+    String composition;
+    int weight;//вес
+    int nutritionalValue;//пищевая ценность
+    double protein;//белки
+    double fat;// жиры
+    double carbohydrates;// углеводы
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        Burger burger = new Burger();
+        burger.name = "Чизбургер";
+        burger.picture = "https://img2.freepng.ru/20180409/biq/kisspng-cheeseburger" +
+                         "-hamburger-chicken-fingers-chicken-san-chese-5acb233ee65db9" +
+                         ".4199963715232622709436.jpg";
+        burger.composition = "Пряный горчичный соус, кетчуп," +
+                             " сочные стрипсы в оригинальной" +
+                             " панировке, лук, сыр Чеддер, " +
+                             "огурцы на пшеничной булочке с " +
+                             "кукурузной посыпкой.";
+        burger.weight = 167;
+        burger.nutritionalValue=237;
+        burger.protein=13.2;
+        burger.fat=8.6;
+        burger.carbohydrates=26.6;
+        System.out.println(burger.name);
+        System.out.println(burger.picture);
+        System.out.println(burger.weight+" Г");
+        System.out.println(burger.composition);
+        System.out.println(burger.nutritionalValue+" Ккал");
+        System.out.println(burger.protein+" Г  "+burger.fat+" Г  "+burger.carbohydrates+" Г");
+
+    }
+}
